7285ab005880c8d1c987533bf46a9328ee9a9cbf
AlexandFunduk <kriptograff56@gmail.com>
2020-12-03 15:03:40 +0300

    UserRestControllers

---
+++ src/main/java/ru/alexandrfunduk/vote/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ru.alexandrfunduk.vote.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/alexandrfunduk/vote/web/user/AbstractUserController.java
@@ -0,0 +1,55 @@
+package ru.alexandrfunduk.vote.web.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import ru.alexandrfunduk.vote.model.User;
+import ru.alexandrfunduk.vote.repository.UserRepository;
+
+import java.util.List;
+
+import static ru.alexandrfunduk.vote.util.ValidationUtil.*;
+
+
+public abstract class AbstractUserController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> getAll() {
+        log.info("getAll");
+        return repository.getAll();
+    }
+
+    public User get(int id) {
+        log.info("get {}", id);
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public User create(User user) {
+        log.info("create {}", user);
+        Assert.notNull(user, "user must not be null");
+        checkNew(user);
+        return repository.save(user);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public void update(User user, int id) {
+        log.info("update {} with id={}", user, id);
+        assureIdConsistent(user, id);
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.id());
+    }
+
+    public User getByMail(String email) {
+        log.info("getByEmail {}", email);
+        Assert.notNull(email, "email must not be null");
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+}
