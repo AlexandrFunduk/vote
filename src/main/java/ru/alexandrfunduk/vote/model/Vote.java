8c44eb2b2f7bff385e576848fbe45c4409e6fedd
AlexandFunduk <kriptograff56@gmail.com>
2020-12-10 16:07:43 +0300

    Vote fetch LAZY

--- src/main/java/ru/alexandrfunduk/vote/model/Vote.java
+++ src/main/java/ru/alexandrfunduk/vote/model/Vote.java
@@ -9,7 +9,7 @@ public class Vote extends AbstractBaseEntity {
     @Column(name = "dateTime")
     LocalDateTime dateTime;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     User user;
 
