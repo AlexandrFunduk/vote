468c6fe51130d90cc894f4433db3946482ff311c
AlexandFunduk <titovnga@gmail.com>
2020-12-23 10:29:21 +0300

    Add test for UserService

---
+++ src/test/java/ru/alexandrfunduk/vote/service/UserServiceTest.java
@@ -0,0 +1,116 @@
+package ru.alexandrfunduk.vote.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.alexandrfunduk.vote.TimingExtension;
+import ru.alexandrfunduk.vote.UserTestData;
+import ru.alexandrfunduk.vote.model.Role;
+import ru.alexandrfunduk.vote.model.User;
+import ru.alexandrfunduk.vote.util.ValidationUtil;
+import ru.alexandrfunduk.vote.util.exception.NotFoundException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.alexandrfunduk.vote.UserTestData.*;
+
+@SpringJUnitConfig(locations = {
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ExtendWith(TimingExtension.class)
+class UserServiceTest {
+    @Autowired
+    protected UserService service;
+
+    @Test
+    void create() {
+        User created = service.create(getNew());
+        int newId = created.id();
+        User newUser = getNew();
+        newUser.setId(newId);
+        USER_MATCHER.assertMatch(created, newUser);
+        USER_MATCHER.assertMatch(service.get(newId), newUser);
+    }
+
+    @Test
+    void duplicateMailCreate() {
+        assertThrows(DataAccessException.class, () ->
+                service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.USER)));
+    }
+
+    @Test
+    void delete() {
+        service.delete(USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(USER_ID));
+    }
+
+    @Test
+    void deletedNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND));
+    }
+
+    @Test
+    void get() {
+        User user = service.get(ADMIN_ID);
+        USER_MATCHER.assertMatch(user, UserTestData.admin);
+    }
+
+    @Test
+    void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND));
+    }
+
+    @Test
+    void getByEmail() {
+        User user = service.getByEmail("admin@gmail.com");
+        USER_MATCHER.assertMatch(user, admin);
+    }
+
+    @Test
+    void update() {
+        User updated = getUpdated();
+        service.update(updated);
+        USER_MATCHER.assertMatch(service.get(USER_ID), getUpdated());
+    }
+
+    @Test
+    void getAll() {
+        List<User> all = service.getAll();
+        USER_MATCHER.assertMatch(all, admin, user, user1, user2);
+    }
+
+    @Test
+    void createWithException() {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.USER)), ConstraintViolationException.class);
+    }
+
+    @Test
+    void enable() {
+        service.enable(USER_ID, false);
+        assertFalse(service.get(USER_ID).isEnabled());
+        service.enable(USER_ID, true);
+        assertTrue(service.get(USER_ID).isEnabled());
+    }
+
+
+    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
+    protected <T extends Throwable> void validateRootCause(Runnable runnable, Class<T> rootExceptionClass) {
+        assertThrows(rootExceptionClass, () -> {
+            try {
+                runnable.run();
+            } catch (Exception e) {
+                throw ValidationUtil.getRootCause(e);
+            }
+        });
+    }
+}
